689e7e98cacc4f127291a1c7e1b630e0db2e1dbb
Maria <m2004brazlira@outlook.com>
2025-02-28 21:02:58 -0300

    third commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.err.println("How are you?");
+        System.out.println("How are you?");
+        System.out.println("I'm fine, thank you.");
     }
 }
